3a05e9567890e17006239662b3815e4688d7cd4e
Takashi Mori <kanetugu2015@gmail.com>
2023-11-21 15:03:44 +0900

    add remote ipaddr for airsim

--- px4/sim/simstart.bash
+++ px4/sim/simstart.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 export PX4_SIM_HOST_ADDR=`cat /etc/hosts | grep $DOCKER_LOCAL_HOST_ADDR | awk '{print $1}'`
+if [ $# -eq 1 ]
+then
+    export PX4_SIM_HOST_ADDR=${1}
+fi
+
 make px4_sitl none_iris
