3d3dd5d4c25b1ba56e2cf7ad8b567385940ea3dc
Takashi Mori <tmori@esm.co.jp>
2023-12-03 12:13:46 +0900

    bug fixed for linux

--- hakoniwa/python/graph.py
+++ hakoniwa/python/graph.py
@@ -6,9 +6,10 @@ df = pd.read_csv('height_data.csv')
 
 # グラフの描画
 plt.figure(figsize=(10, 6))  # グラフのサイズ指定
-plt.plot(df['Time'], df['Height'])  # 時間に対する高さをプロット
+plt.plot(df['Time'].to_numpy(), df['Height'].to_numpy())  # 時間と高さをNumPy配列に変換してプロット
 plt.title('Height over Time')  # グラフのタイトル
 plt.xlabel('Time')  # x軸のラベル
 plt.ylabel('Height')  # y軸のラベル
 plt.grid(True)  # グリッドの表示
 plt.show()  # グラフの表示
+
