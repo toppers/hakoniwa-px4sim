3788c425eefd2fde87b62cb83ff79d6a7ee4a093
kenjihiranabe <kenji.hiranabe@change-vision.com>
2023-12-11 15:06:38 +0900

    new directory and make const&

--- hakoniwa/src/assets/drone/physics/math_and_physics/math_and_physics.hpp
+++ hakoniwa/src/assets/drone/physics/math_and_physics/math_and_physics.hpp
@@ -10,16 +10,34 @@ AngleType, AngularVelocityType, AngularAccelerationType;
 /* maths */
 /* think about the sin/cos cache later... check working right first */
 
-VelocityType velocity_body_to_ground(VelocityType body, AngleType angle);
-VelocityType velocity_ground_to_body(VelocityType ground, AngleType angle);
+VelocityType velocity_body_to_ground(
+    const VelocityType& body,
+    const AngleType& angle);
+VelocityType velocity_ground_to_body(
+    const VelocityType& ground,
+    const AngleType& angle);
 
-AngularVelocityType angular_velocity_body_to_ground(AngularVelocityType angular_veleocy, AngleType angle);
-AngularVelocityType angular_velocity_ground_to_body(AngularVelocityType angular_velocity_ground_frame, AngleType angle);
+AngularVelocityType angular_velocity_body_to_ground(
+    const AngularVelocityType& angular_veleocy,
+    const AngleType& angle);
+AngularVelocityType angular_velocity_ground_to_body(
+    const AngularVelocityType& angular_velocity_ground_frame,
+    const AngleType& angle);
 
 /* physics */
+AccelerationType Acceleration_in_body_frame(
+    const VelocityType& body,
+    const AngleType& angle,
+    double thrust, double mass /* 0 is not allowed */, double gravity, double drag);
 
-AccelerationType Acceleration_in_body_frame(VelocityType body, AngleType angle, double thrust, double mass);
+AccelerationType Acceleration_in_ground_frame(
+    const VelocityType& body,
+    const AngleType& angle,
+    double thrust, double mass /* 0 is not allowed */, double gravity, double drag);
 
-AngularAccelerationType Angular_acceleration_in_body_frame(AngularVelocityType angular_velocity, AngleType angle, double trust, double mass, double inertia_x, double inertia_y, double inertia_z);
+AngularAccelerationType Angular_acceleration_in_body_frame(
+    const AngularVelocityType& angular_velocity,
+    const AngleType& angle,
+    double trust, double mass, double inertia_x, double inertia_y, double inertia_z);
 
 #endif /* MATH_AND_PHYSICS_HPP_ */
