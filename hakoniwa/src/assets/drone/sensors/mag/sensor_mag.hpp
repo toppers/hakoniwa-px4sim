8d766577eac9b7ac63555bb6787c9a898b59f22d
Takashi Mori <kanetugu2015@gmail.com>
2023-12-26 08:13:01 +0900

    updated for latest

--- hakoniwa/src/assets/drone/sensors/mag/sensor_mag.hpp
+++ hakoniwa/src/assets/drone/sensors/mag/sensor_mag.hpp
@@ -91,7 +91,7 @@ public:
 
     void run(const DroneAngleType& angle) override
     {
-        run_old(angle);
+        run_new(angle);
         total_time_sec += delta_time_sec;
     }
     DroneMagDataType sensor_value() override
