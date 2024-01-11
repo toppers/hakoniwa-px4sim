0777e75d92ae05a83a3e2cabf99e535a09ae9b60
Takashi Mori <kanetugu2015@gmail.com>
2024-01-11 09:34:21 +0900

    updated jmavsim logic

--- hakoniwa/src/assets/drone/physics/rotor/rotor_dynamics_jmavsim.hpp
+++ hakoniwa/src/assets/drone/physics/rotor/rotor_dynamics_jmavsim.hpp
@@ -15,8 +15,7 @@ private:
     double param_rpm_max = 6000.0;
     double param_tr = 1.0;
     double param_kr = 1.0;
-    DroneRotorSpeedType speed;
-    DroneRotorSpeedType next_speed;
+    double w;
     double delta_time_sec;
     double total_time_sec;
 
@@ -26,7 +25,7 @@ public:
     {
         this->delta_time_sec = dt;
         this->total_time_sec = 0;
-        this->speed.data = 0;
+        this->w = 0;
     }
     void set_params(double rpm_max, double tr, double kr)
     {
@@ -36,30 +35,19 @@ public:
     }
     void set_rotor_speed(DroneRotorSpeedType &rotor_speed) override 
     {
-        this->speed = rotor_speed;
+        this->w = rotor_speed.data / this->param_kr;
     }
     DroneRotorSpeedType get_rotor_speed() const override
     {
         DroneRotorSpeedType value;
-        value.data = this->speed.data;
+        value.data = this->w * this->param_kr;
         return value;
     }
 
     void run(double control) override
     {
-        this->next_speed.data =   (
-                                    drone_physics::rotor_omega_acceleration(param_kr, param_tr, speed.data, control)
-                                  ) * this->delta_time_sec
-                                + this->speed.data;
-        // Cap the next speed at the maximum RPM if it exceeds it
-        if (this->next_speed.data > this->param_rpm_max) {
-            this->next_speed.data = this->param_rpm_max;
-        }
-        // Ensure the next speed does not fall below the minimum RPM (assuming 0 for this example)
-        else if (this->next_speed.data < 0) {
-            this->next_speed.data = 0;
-        }
-        this->speed.data = this->next_speed.data;
+        //w += (control - w) * (1.0 - Math.exp(-dt / tau));
+        this->w += (control - this->w) * (1.0 - exp(-this->delta_time_sec/ this->param_tr));
         this->total_time_sec += this->delta_time_sec;
     }
     const std::vector<std::string> log_head() override
