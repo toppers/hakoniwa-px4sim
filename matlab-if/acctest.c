d39e41f864b78e8a58181cdc58dca6d6f468458e
kenjihiranabe <kenji.hiranabe@change-vision.com>
2024-01-17 09:11:23 +0900

    added a test.

---
+++ matlab-if/acctest.c
@@ -0,0 +1,34 @@
+/* unit test for acceleration in matlab */
+#include <stdio.h>
+#include <assert.h>
+
+#include "drone_physics_matlab.h"
+
+int main() {
+    mi_drone_acceleration_in_t in;
+    in.phi = 0.0;
+    in.theta = 0.0;
+    in.psi = 0.0;
+    in.u = 0.0;
+    in.v = 0.0;
+    in.w = 0.0;
+    in.p = 0.0;
+    in.q = 0.0;
+    in.r = 0.0;
+    in.thrust = 0.0;
+    in.torque_x = 0.0;
+    in.torque_y = 0.0;
+    in.torque_z = 0.0;
+
+    in.mass = 1.0;
+    in.I_xx = 1.0;
+    in.I_yy = 1.0;
+    in.I_zz = 1.0;
+    in.gravity = 9.81;
+    in.drag = 0.0;
+
+    mi_drone_acceleration_out_t out = mi_drone_acceleration(&in);
+    assert(out.ddx == 0.0);
+    
+    return 0;
+}
