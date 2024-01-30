3077ae70bfdb432187acec50b284c79459f69ee3
kenjihiranabe <kenji.hiranabe@change-vision.com>
2024-01-30 15:55:15 +0900

    added roundtrip test for matlab<->physics

--- matlab-if/acctest.cpp
+++ matlab-if/acctest.cpp
@@ -90,8 +90,49 @@ static void test_first_case() {
     assert_almost_equal(out_m.dv, expected.dv);
 }
 
+void round_test()
+{
+    mi_drone_acceleration_in_t in;
+    in.phi = 0.0;    in.theta = 0.0;    in.psi = 0.0;
+    in.u = 0.0;    in.v = 0.0;    in.w = 0.0;
+    in.p = 0.0;    in.q = 0.0;    in.r = 0.0;
+    in.thrust = 0.0;
+    in.torque_x = 0.0;    in.torque_y = 0.0;    in.torque_z = 0.0;
+    in.mass = 1.0;
+    in.Ixx = 1.0;    in.Iyy = 2.0;    in.Izz = 3.0;
+    in.gravity = 9.81;
+    in.drag = 0.0;
+
+    for (double i = 0; i < M_PI; i += (M_PI/180)*30) {
+        for (double j = 0; j < M_PI/2; j+= (M_PI/180)*30) {
+            for (double k = 0; k < M_PI; k+= (M_PI/180)*60) {
+                in.phi = i; in.theta = j; in.psi = k;
+                in.u = 1.0; in.v = 2.0; in.w = 3.0;
+                in.p = 1.0; in.q = 2.0; in.r = 3.0;
+                in.thrust = 1.0;
+                in.torque_x = 10.0; in.torque_y = 20.0; in.torque_z = 30.0;
+                in.mass = 10.0;
+                in.Ixx = 2.0; in.Iyy = 5.0; in.Izz = 8.0;
+                in.gravity = 9.81;
+                in.drag = 2.0;
+                mi_drone_acceleration_out_t out_m = mi_drone_acceleration(&in);
+                mi_drone_acceleration_out_t out_p = drone_acceleration_by_physics(&in);
+                assert_almost_equal(out_m.du, out_p.du);
+                assert_almost_equal(out_m.dv, out_p.dv);
+                assert_almost_equal(out_m.dw, out_p.dw);
+                assert_almost_equal(out_m.dp, out_p.dp);
+                assert_almost_equal(out_m.dq, out_p.dq);
+                assert_almost_equal(out_m.dr, out_p.dr);
+                fprintf(stderr, ".");
+            }
+        }
+    }
+
+}
+
 int main() {
     T(test_first_case);
+    T(round_test);
     return 0;
 }
 
