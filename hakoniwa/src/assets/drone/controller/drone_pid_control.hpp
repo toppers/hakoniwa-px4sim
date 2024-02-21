f6dca179bc49d0202508ba6d6418f9456b90cf95
Takashi Mori <kanetugu2015@gmail.com>
2024-02-22 08:59:20 +0900

    fix compile error for win

--- hakoniwa/src/assets/drone/controller/drone_pid_control.hpp
+++ hakoniwa/src/assets/drone/controller/drone_pid_control.hpp
@@ -5,40 +5,32 @@
 
 #include <iostream>
 #include <string>
-#include "../../../utils/simple_pid.hpp"  // PIDクラスをインクルード
-#include "../../../utils/csv_data.hpp"  // CsvDataクラスをインクルード
+#include "../../../utils/simple_pid.hpp"
+#include "../../../utils/csv_data.hpp"
 
 class DronePidControl {
 private:
-    // PIDコントローラのインスタンス
     PID pid_control;
 
-    // CSVデータ記録用のインスタンス
     CsvData csv_data;
 
 public:
-    // コンストラクタ
     DronePidControl(double Kp, double Ki, double Kd, double setpoint, 
                     const std::string& csv_file_name, const std::vector<std::string>& csv_header)
         : pid_control(Kp, Ki, Kd, setpoint),
           csv_data(csv_file_name, csv_header) {}
-
-    // PID制御値を計算するメソッド
     double calculate(double input) {
         return pid_control.calculate(input);
     }
 
-    // CSVファイルにデータを書き込むメソッド
     void write_to_csv(const std::vector<std::string>& data) {
         csv_data.write(data);
     }
 
-    // CSVファイルをフラッシュするメソッド
     void flush_csv() {
         csv_data.flush();
     }
 
-    // PID制御目標値を設定するメソッド
     void set_setpoint(double setpoint) {
         pid_control.set_setpoint(setpoint);
     }
