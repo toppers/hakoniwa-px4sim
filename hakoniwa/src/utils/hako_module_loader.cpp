0c4b716d5bd924bedd7dff98630659014548a1e5
Takashi Mori <kanetugu2015@gmail.com>
2024-02-22 09:33:20 +0900

    add dynamic loading for win

--- hakoniwa/src/utils/hako_module_loader.cpp
+++ hakoniwa/src/utils/hako_module_loader.cpp
@@ -1,7 +1,45 @@
 #include "hako_module_loader.hpp"
-#include <dlfcn.h>
 #include <iostream>
 
+#ifdef WIN32
+#include <windows.h>
+typedef HakoModuleHeaderType* (*PFN_GetModuleHeader)();
+void* hako_module_handle(const char* filepath, HakoModuleHeaderType** header) {
+    HMODULE handle = LoadLibrary(filepath);
+    if (handle == nullptr) {
+        std::cerr << "ERROR: can not load module filepath: " << filepath << std::endl;
+        return nullptr;
+    }
+    auto pfnGetModuleHeader = reinterpret_cast<PFN_GetModuleHeader>(GetProcAddress(handle, HAOKO_MODULE_HEADER_NAME));
+    if (pfnGetModuleHeader == nullptr) {
+        std::cerr << "ERROR: cannot find symbol: " << HAOKO_MODULE_HEADER_NAME << " on module filepath: " << filepath << std::endl;
+        FreeLibrary(handle);
+        return nullptr;
+    }
+
+    *header = pfnGetModuleHeader();
+    if ((*header)->magicno != HAKO_MODULE_MAGICNO) {
+        std::cerr << "ERROR: invalid magicno: " << (*header)->magicno << " on module filepath: " << filepath << std::endl;
+        return nullptr;
+    }
+    if ((*header)->version != HAKO_MODULE_VERSION) {
+        std::cerr << "ERROR: invalid version: " << (*header)->version << " on module filepath: " << filepath << std::endl;
+        return nullptr;
+    }
+    return handle;
+}
+
+void* hako_module_load_symbol(void* handle, const char* symbol_name) {
+    auto* varp = GetProcAddress(static_cast<HMODULE>(handle), symbol_name);
+    if (varp == nullptr) {
+        std::cerr << "ERROR: can not find symbol: " << symbol_name << std::endl;
+        return nullptr;
+    }
+    return varp;
+}
+
+#else
+#include <dlfcn.h>
 void *hako_module_handle(const char* filepath, HakoModuleHeaderType** header)
 {
     void *handle = dlopen(filepath, RTLD_NOW);
@@ -35,3 +73,4 @@ void *hako_module_load_symbol(void *handle, const char* symbole_name)
     }
     return varp;
 }
+#endif
