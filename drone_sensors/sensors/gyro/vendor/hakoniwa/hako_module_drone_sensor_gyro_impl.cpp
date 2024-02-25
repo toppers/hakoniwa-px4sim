56d09c0c5489b7e261ca2dfd519573857bd8c5bc
Takashi Mori <kanetugu2015@gmail.com>
2024-02-26 08:34:17 +0900

    add logic

--- drone_sensors/sensors/gyro/vendor/hakoniwa/hako_module_drone_sensor_gyro_impl.cpp
+++ drone_sensors/sensors/gyro/vendor/hakoniwa/hako_module_drone_sensor_gyro_impl.cpp
@@ -5,14 +5,31 @@ const char* hako_module_drone_sensor_gyro_impl_get_name(void)
     return "HakoniwaGyroSensor";
 }
 
-int hako_module_drone_sensor_gyro_impl_init(void* context)
+
+int hako_module_drone_sensor_gyro_impl_init(void*)
 {
-    //TODO
     return 0;
+}
+#define TEMP_MIN    10.0
+#define TEMP_MAX    20.0
+#define DRIFT       0.01
+/*
+ * data: rad/sec
+ */
+static double noise(double data, double temperature)
+{
+    if (temperature >= TEMP_MIN && temperature <= TEMP_MAX) {
+        return data;
+    }
+    return data + (temperature * DRIFT);
 }
 
 mi_drone_sensor_gyro_out_t hako_module_drone_sensor_gyro_impl_run(mi_drone_sensor_gyro_in_t *in)
 {
-    mi_drone_sensor_gyro_out_t sensor_gyro_output;
-    return sensor_gyro_output;
+    mi_drone_sensor_gyro_out_t out;
+    double temperature = in->disturbance.d_temp.value;
+    out.angular_velocity_x = noise(in->angular_velocity_x, temperature);
+    out.angular_velocity_y = noise(in->angular_velocity_y, temperature);
+    out.angular_velocity_z = noise(in->angular_velocity_z, temperature);
+    return out;
 }
