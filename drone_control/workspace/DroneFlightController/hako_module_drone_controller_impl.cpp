bd0b7af1d686be399f7c315e9af4012149b0a23e
Takashi Mori <kanetugu2015@gmail.com>
2024-03-02 10:10:32 +0900

    refactored

--- drone_control/workspace/DroneFlightController/hako_module_drone_controller_impl.cpp
+++ drone_control/workspace/DroneFlightController/hako_module_drone_controller_impl.cpp
@@ -75,6 +75,65 @@ typedef enum {
 } DroneControlModeType;
 static DroneControlModeType drone_control_mode = DRONE_CONTROL_MODE_NONE;
 
+/*
+ * power: 
+ *  forward: 0..100
+ *  back   : -100..0
+ */
+#define MAX_FORWARD_DEGREE 10.0
+static double move_forward(double power, double q, EulerType &euler)
+{
+    double target_angle_degree = 0;
+    power = get_limit_value(power, 0, -100, 100);
+    if (power > 0) {
+        target_angle_degree = -get_limit_value(power/10.0, 0, 0, MAX_FORWARD_DEGREE);
+    }
+    else {
+        target_angle_degree = -get_limit_value(power/10.0, 0, -MAX_FORWARD_DEGREE, 0);
+    }
+    double target_pitch = DEGREE2RADIAN(target_angle_degree);
+    double target_pitch_rate_max = RPM2EULER_RATE(2);
+    double target_pitch_rate = pid_ctrl_pitch_angle->run(target_pitch, euler);
+    target_pitch_rate = get_limit_value(target_pitch_rate, 0, -target_pitch_rate_max, target_pitch_rate_max);
+    double torque_y = pid_ctrl_pitch_rate->run(target_pitch_rate, NORMALIZE_RADIAN(q));
+    torque_y = get_limit_value(torque_y, 0, -M_PI/10.0, M_PI/10.0);
+    return torque_y;
+}
+/*
+ * power: 
+ *  right: 100..0
+ *  left : 0..-100
+ */
+#define MAX_HORIZONTAL_DEGREE 10.0
+static double move_horizontal(double power, double p, EulerType &euler)
+{
+    double target_roll_rate_max = RPM2EULER_RATE(10);
+    double target_angle_degree = 0;
+    power = get_limit_value(power, 0, -100, 100);
+    if (power > 0) {
+        target_angle_degree = -get_limit_value(power/10.0, 0, 0, MAX_HORIZONTAL_DEGREE);
+    }
+    else {
+        target_angle_degree = -get_limit_value(power/10.0, 0, -MAX_HORIZONTAL_DEGREE, 0);
+    }
+    double target_roll = DEGREE2RADIAN(target_angle_degree);
+    double target_roll_rate = pid_ctrl_roll_angle->run(target_roll, euler);
+    target_roll_rate = get_limit_value(target_roll_rate, 0, -target_roll_rate_max, target_roll_rate_max);
+    double torque_x = pid_ctrl_roll_rate->run(target_roll_rate, NORMALIZE_RADIAN(p));
+    torque_x = get_limit_value(torque_x, 0, -M_PI/10.0, M_PI/10.0);
+    return torque_x;
+}
+
+static double rotate_yaw(double target_yaw, double r, EulerType &euler)
+{
+    double target_yaw_rate_max = RPM2EULER_RATE(20);
+    double target_yaw_rate = pid_ctrl_yaw_angle->run(target_yaw, euler);
+    target_yaw_rate = get_limit_value(target_yaw_rate, 0, -target_yaw_rate_max, target_yaw_rate_max);
+    double torque_z = pid_ctrl_yaw_rate->run(target_yaw_rate, NORMALIZE_RADIAN(r));
+    torque_z = get_limit_value(torque_z, 0, -M_PI/4.0, M_PI/4.0);
+    return torque_z;
+}
+
 mi_drone_control_out_t hako_module_drone_controller_impl_run(mi_drone_control_in_t *in)
 {
     /*
@@ -82,11 +141,6 @@ mi_drone_control_out_t hako_module_drone_controller_impl_run(mi_drone_control_in
      */
     mi_drone_control_out_t control_output;
     double target_yaw = DEGREE2RADIAN(45);
-    double target_yaw_rate_max = RPM2EULER_RATE(10);
-    double target_roll = DEGREE2RADIAN(0);
-    double target_roll_rate_max = RPM2EULER_RATE(10);
-    double target_pitch = DEGREE2RADIAN(-5);
-    double target_pitch_rate_max = RPM2EULER_RATE(1);
     EulerType euler = {NORMALIZE_RADIAN(in->euler_x), NORMALIZE_RADIAN(in->euler_y), NORMALIZE_RADIAN(in->euler_z)};
     if (drone_control_mode == DRONE_CONTROL_MODE_NONE) {
         in->target_pos_z = -10;
@@ -125,21 +179,11 @@ mi_drone_control_out_t hako_module_drone_controller_impl_run(mi_drone_control_in
     // Yaw control
     if (drone_control_mode >= DRONE_CONTROL_MODE_YAW) {
         static int target_done_count = 0;
-        
-        double target_yaw_rate = pid_ctrl_yaw_angle->run(target_yaw, euler);
-        target_yaw_rate = get_limit_value(target_yaw_rate, 0, -target_yaw_rate_max, target_yaw_rate_max);
-        control_output.torque_z = pid_ctrl_yaw_rate->run(target_yaw_rate, NORMALIZE_RADIAN(in->r));
-        control_output.torque_z = get_limit_value(control_output.torque_z, 0, -M_PI/10.0, M_PI/10.0);
-        //std::cout << "target_yaw: " << target_yaw << std::endl;
-        //std::cout << "current_yaw: " << euler.psi << std::endl;
-        //std::cout << "target_yaw_rate: " << target_yaw_rate << std::endl;
-        //std::cout << "current_yaw_rate: " << NORMALIZE_RADIAN(in->r) << std::endl;
-        //std::cout << "control_output.torque_z: " << control_output.torque_z << std::endl;
+        control_output.torque_z = rotate_yaw(target_yaw, in->r, euler);
         if (drone_control_mode == DRONE_CONTROL_MODE_YAW) {
             if (ALMOST_EQUAL(target_yaw, euler.psi, DEGREE2RADIAN(1))) {
                 target_done_count++;
             }
-
             if (target_done_count >= 10) {
                 drone_control_mode = DRONE_CONTROL_MODE_MOVE;
                 std::cout << "INFO: start move mode" << std::endl;
@@ -147,22 +191,11 @@ mi_drone_control_out_t hako_module_drone_controller_impl_run(mi_drone_control_in
         }
     }
     if (drone_control_mode >= DRONE_CONTROL_MODE_MOVE) {
-        double target_pitch_rate = pid_ctrl_pitch_angle->run(target_pitch, euler);
-        target_pitch_rate = get_limit_value(target_pitch_rate, 0, -target_pitch_rate_max, target_pitch_rate_max);
-        control_output.torque_y = pid_ctrl_pitch_rate->run(target_pitch_rate, NORMALIZE_RADIAN(in->q));
-        control_output.torque_y = get_limit_value(control_output.torque_y, 0, -M_PI/10.0, M_PI/10.0);
-        //std::cout << "target_pitch: " << target_pitch << std::endl;
-        //std::cout << "current_pitch: " << euler.theta << std::endl;
-        //std::cout << "target_pitch_rate: " << target_pitch_rate << std::endl;
-        //std::cout << "current_pitch_rate: " << NORMALIZE_RADIAN(in->q) << std::endl;
-        //std::cout << "control_output.torque_y: " << control_output.torque_y << std::endl;
+        control_output.torque_y = move_forward(100, in->q, euler);
     }
     //roll control
     {
-        double target_roll_rate = pid_ctrl_roll_angle->run(target_roll, euler);
-        target_roll_rate = get_limit_value(target_roll_rate, 0, -target_roll_rate_max, target_roll_rate_max);
-        control_output.torque_x = pid_ctrl_roll_rate->run(target_roll_rate, NORMALIZE_RADIAN(in->p));
-        control_output.torque_x = get_limit_value(control_output.torque_x, 0, -M_PI/10.0, M_PI/10.0);
+        control_output.torque_x = move_horizontal(0, in->p, euler);
     }
 
     return control_output;
