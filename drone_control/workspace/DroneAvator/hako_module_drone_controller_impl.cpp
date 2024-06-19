92f86813dd22a9e88dd87101f0e967c0d43a03b4
Takashi Mori <kanetugu2015@gmail.com>
2024-06-19 09:09:12 +0900

    bug fixed

--- drone_control/workspace/DroneAvator/hako_module_drone_controller_impl.cpp
+++ drone_control/workspace/DroneAvator/hako_module_drone_controller_impl.cpp
@@ -2,30 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 const char* hako_module_drone_controller_impl_get_name(void)
 {
     return "DroneAvator";
 }
 
-static double Kp = 10;
-static double Ki = 0.0;
-static double Kd = 0.0;
 
-static double integral = 0.0;
-static double prev_error = 0.0;
+typedef struct {
+    double Kp;
+    double Ki;
+    double Kd;
+    double integral;
+    double prev_error;
+    char name[256];
+} MyContextType;
 
-void* hako_module_drone_controller_impl_create_context(void*)
+void* hako_module_drone_controller_impl_create_context(void* argp)
 {
-    return nullptr;
+    auto* context = new MyContextType();
+    const char* ptr = (const char*)argp;
+    memset(context->name, 0, sizeof(context->name));
+    strcpy(context->name, ptr);
+    printf("my name is %s\n", context->name);
+    return (void*)context;
 }
 int hako_module_drone_controller_impl_is_operation_doing(void*)
 {
     return true;
 }
 
-int hako_module_drone_controller_impl_init(void* context)
+int hako_module_drone_controller_impl_init(void* argp)
 {
+    MyContextType* context = (MyContextType*)argp;
     char cwd[1024];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
         printf("Current working dir: %s\n", cwd);
@@ -41,33 +51,50 @@ int hako_module_drone_controller_impl_init(void* context)
     }
     
     char line[256];
+    char param_name[256];
+    char name[256];
+    char param_type[3];
+    double param_value;
+
     while (fgets(line, sizeof(line), file)) {
-        if (sscanf(line, "DroneAvator_Kp=%lf", &Kp) == 1) continue;
-        if (sscanf(line, "DroneAvator_Ki=%lf", &Ki) == 1) continue;
-        if (sscanf(line, "DroneAvator_Kd=%lf", &Kd) == 1) continue;
+        if (sscanf(line, "%[^_]_%2s=%lf", name, param_type, &param_value) == 3) {
+            if (strcmp(name, context->name) == 0) {
+                if (strcmp(param_type, "Kp") == 0) {
+                    context->Kp = param_value;
+                } else if (strcmp(param_type, "Ki") == 0) {
+                    context->Ki = param_value;
+                } else if (strcmp(param_type, "Kd") == 0) {
+                    context->Kd = param_value;
+                }
+            }
+        }
     }
-    
+    printf("DroneController Name: %s Kp=%lf Kd=%lf Ki=%lf\n", 
+            context->name, 
+            context->Kp, context->Kd, context->Ki);
+
     fclose(file);
 
-    integral = 0.0;
-    prev_error = 0.0;
+    context->integral = 0.0;
+    context->prev_error = 0.0;
     return 0;
 }
-#define THRUST_MAX (9.81 * 1.1)
+#define THRUST_MAX (9.81 * 100.0)
 #define THRUST_MIN (0.0)
 mi_drone_control_out_t hako_module_drone_controller_impl_run(mi_drone_control_in_t *in)
 {
+    MyContextType* context = (MyContextType*)in->context;
     const double target_altitude = 10.0;
     
     double error = target_altitude + in->pos_z;
     
-    integral += error * 0.003;
+    context->integral += error * 0.003;
     
-    double derivative = (error - prev_error) / 0.003;
+    double derivative = (error - context->prev_error) / 0.003;
     
-    double output = Kp * error + Ki * integral + Kd * derivative;
+    double output = context->Kp * error + context->Ki * context->integral + context->Kd * derivative;
     
-    prev_error = error;
+    context->prev_error = error;
 
     if (output > THRUST_MAX) {
         output = THRUST_MAX;
