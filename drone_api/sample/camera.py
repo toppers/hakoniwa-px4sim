63aa3fd3634ddbcf4ac53ad901fb3a43dc1982e8
Takashi Mori <kanetugu2015@gmail.com>
2024-07-21 12:58:02 +0900

    add error handling

--- drone_api/sample/camera.py
+++ drone_api/sample/camera.py
@@ -6,25 +6,34 @@ import math
 import numpy as np
 import cv2
 
-def image_display_thread(client, fps=30):
+def image_display_thread(client, fps=15):
     interval = 1.0 / fps
     while True:
         start_time = time.time()
 
-        response = client.simGetImage("0", hakosim.ImageType.Scene)
-        if response:
-            img_np = np.frombuffer(response, dtype=np.uint8)
-            img_rgb = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-            cv2.imshow("Camera View", img_rgb)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        try:
+            response = client.simGetImage("0", hakosim.ImageType.Scene)
+            if response:
+                img_np = np.frombuffer(response, dtype=np.uint8)
+                img_rgb = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+
+                if img_rgb is None or img_rgb.size == 0:
+                    print("Error: Failed to decode image")
+                else:
+                    cv2.imshow("Camera View", img_rgb)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+            else:
+                print("Error: No image received from client")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
         elapsed_time = time.time() - start_time
         wait_time = max(0, interval - elapsed_time)
         time.sleep(wait_time)
 
     cv2.destroyAllWindows()
-
 
 
 def main():
