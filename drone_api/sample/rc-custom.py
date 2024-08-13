a5f4adaf934473748a99863452d048ed20219f68
Takashi Mori <kanetugu2015@gmail.com>
2024-08-13 14:58:13 +0900

    add custom rc

---
+++ drone_api/sample/rc-custom.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import hakosim
+import pygame
+import time
+import os
+from rc_utils.rc_utils import RcConfig, StickMonitor
+
+# デフォルトのJSONファイルパス
+DEFAULT_CONFIG_PATH = "rc_config/ps4-control.json"
+
+def saveCameraImage(client):
+    png_image = client.simGetImage("0", hakosim.ImageType.Scene)
+    if png_image:
+        with open("scene.png", "wb") as f:
+            f.write(png_image)
+
+def joystick_control(client: hakosim.MultirotorClient, joystick, stick_monitor: StickMonitor):
+    try:
+        while True:
+            data = client.getGameJoystickData()
+            for event in pygame.event.get():
+                if event.type == pygame.JOYAXISMOTION:
+                    if event.axis < 4:
+                        op_index = stick_monitor.rc_config.get_op_index(event.axis)
+                        stick_value = stick_monitor.stick_value(event.axis, event.value)
+                        if (abs(stick_value) > 0.1):
+                            print(f"stick event: stick_index={event.axis} op_index={op_index} event.value={event.value} stick_value={stick_value}")
+                        data['axis'] = list(data['axis'])
+                        data['axis'][op_index] = stick_value
+                    else:
+                        print(f'ERROR: not supported axis index: {event.axis}')
+                elif event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP:
+                    if event.button < 16:
+                        data['button'] = list(data['button'])
+                        event_op_index = stick_monitor.rc_config.get_event_op_index(event.button)
+                        event_triggered = stick_monitor.switch_event(event.button, (event.type == pygame.JOYBUTTONDOWN))
+                        print(f"button event: switch_index={event.button} event_op_index={event_op_index} down: {(event.type == pygame.JOYBUTTONDOWN)} event_triggered={event_triggered}")
+                        data['button'][event_op_index] = event_triggered
+                        if event_triggered:
+                            if event_op_index == stick_monitor.rc_config.SWITCH_CAMERA_SHOT:
+                                time.sleep(0.5)
+                                saveCameraImage(client)
+                    else:
+                        print(f'ERROR: not supported button index: {event.button}')
+            client.putGameJoystickData(data)
+    except KeyboardInterrupt:
+        pygame.joystick.quit()
+        pygame.quit()
+
+def main():
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <config_path> [rc_config_path]")
+        return 1
+    
+    config_path = sys.argv[1]
+    rc_config_path = 'rc_config/ps4-control.json'
+    if (len(sys.argv) == 3):
+        rc_config_path = sys.argv[2]
+    
+    if not os.path.exists(config_path):
+        print(f"ERROR: Config file not found at '{config_path}'")
+        return 1
+    if not os.path.exists(rc_config_path):
+        print(f"ERROR: Config file not found at '{rc_config_path}'")
+        return 1
+
+    # RcConfigとStickMonitorの初期化
+    rc_config = RcConfig(rc_config_path)
+    stick_monitor = StickMonitor(rc_config)
+
+    pygame.init()
+    pygame.joystick.init()
+
+    joystick_count = pygame.joystick.get_count()
+    print(f"Number of joysticks: {joystick_count}")
+    try:
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+        print(f'ジョイスティックの名前: {joystick.get_name()}')
+        print(f'ボタン数 : {joystick.get_numbuttons()}')
+    except pygame.error:
+        print('ジョイスティックが接続されていません')
+        pygame.joystick.quit()
+        pygame.quit()
+        return 1
+
+    client = hakosim.MultirotorClient(config_path)
+    client.default_drone_name = "DroneTransporter"
+    client.confirmConnection()
+    client.enableApiControl(True)
+    client.armDisarm(True)
+    joystick_control(client, joystick, stick_monitor)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ drone_api/sample/rc_utils/rc_utils.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+
+class RcConfig:
+    # スティック操作の定数定義
+    STICK_TURN_LR = 0  # Turn Left/Right (Left Stick - LR)
+    STICK_UP_DOWN = 1  # Up/Down (Left Stick - UD)
+    STICK_MOVE_LR = 2  # Move Left/Right (Right Stick - LR)
+    STICK_MOVE_FB = 3  # Move Forward/Back (Right Stick - UD)
+
+    # スイッチイベントの定数定義
+    SWITCH_RADIO_CONTROL_ENABLE = 0
+    SWITCH_GRAB_BAGGAGE =1
+    SWITCH_CAMERA_SHOT = 2
+    
+    def __init__(self, filepath):
+        self.config = self._load_json(filepath)
+
+    def _load_json(self, path):
+        try:
+            with open(path, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print(f"ERROR: File not found '{path}'")
+        except json.JSONDecodeError:
+            print(f"ERROR: Invalid Json fromat '{path}'")
+        except PermissionError:
+            print(f"ERROR: Permission denied '{path}'")
+        except Exception as e:
+            print(f"ERROR: {e}")
+        return None
+
+
+    def get_event_op_index(self, switch_index):
+        """
+        Get the event index based on switch index.
+        
+        Returns the corresponding index.
+        """
+        event_op_map = {
+            self.config['Event']['RadioControlEnable']['index']: self.SWITCH_RADIO_CONTROL_ENABLE,
+            self.config['Event']['GrabBaggage']['index']: self.SWITCH_GRAB_BAGGAGE,
+            self.config['Event']['Camera']['index']: self.SWITCH_CAMERA_SHOT
+        }
+        return event_op_map.get(switch_index, None)
+
+    def get_switch_feature(self, switch_index):
+        """
+        Get the switch feature based on switch index.
+        
+        Returns the corresponding switch feature dictionary.
+        """
+        feature_map = {
+            self.config['Event']['RadioControlEnable']['index']: self.config['Event']['RadioControlEnable'],
+            self.config['Event']['Camera']['index']: self.config['Event']['Camera'],
+            self.config['Event']['GrabBaggage']['index']: self.config['Event']['GrabBaggage']
+        }
+        feature = feature_map.get(switch_index, None)
+        if feature:
+            feature['type'] = feature.get('type', 'toggle')
+            feature['off'] = feature.get('off', 'up')
+            feature['on'] = feature.get('on', 'down')
+        else:
+            raise ValueError(f"Feature for switch index {switch_index} not found.")
+ 
+        return feature
+
+    def get_op_index(self, stick_index):
+        """
+        Get the operation index based on stick index.
+        
+        Returns the corresponding index.
+        """
+        if self.config['mode'] == 2:
+            op_map = {
+                self.config['stick']['Left']['LR']['index']: self.STICK_TURN_LR,
+                self.config['stick']['Left']['UD']['index']: self.STICK_UP_DOWN,
+                self.config['stick']['Right']['LR']['index']: self.STICK_MOVE_LR,
+                self.config['stick']['Right']['UD']['index']: self.STICK_MOVE_FB
+            }
+        else: # mode1
+            op_map = {
+                self.config['stick']['Left']['LR']['index']: self.STICK_TURN_LR,
+                self.config['stick']['Left']['UD']['index']: self.STICK_MOVE_FB,
+                self.config['stick']['Right']['LR']['index']: self.STICK_MOVE_LR,
+                self.config['stick']['Right']['UD']['index']: self.STICK_UP_DOWN
+            }
+
+        return op_map.get(stick_index, None)
+
+    def get_stick_feature(self, stick_index):
+        """
+        Get the stick feature based on stick index.
+        
+        Returns the corresponding stick feature dictionary.
+        """
+        feature_map = {
+            self.config['stick']['Left']['LR']['index']: self.config['stick']['Left']['LR'],
+            self.config['stick']['Left']['UD']['index']: self.config['stick']['Left']['UD'],
+            self.config['stick']['Right']['LR']['index']: self.config['stick']['Right']['LR'],
+            self.config['stick']['Right']['UD']['index']: self.config['stick']['Right']['UD']
+        }
+        feature = feature_map.get(stick_index, None)
+        if feature:
+            feature['conversion'] = feature.get('conversion', None)
+            feature['discretize'] = feature.get('discretize', None)
+            feature['average'] = feature.get('average', False)
+            feature['valueInverse'] = feature.get('valueInverse', False)
+        else:
+            raise ValueError(f"Feature for stick index {stick_index} not found.")
+ 
+        return feature
+
+class StickMonitor:
+    def __init__(self, config: RcConfig):
+        self.rc_config = config
+        self.stick_history = {0: [], 1: [], 2: [], 3: []}
+        self.history_len = 5
+        self.switch_states = {}
+    
+    def stick_value(self, stick_index, stick_value) -> float:
+        op_index = self.rc_config.get_op_index(stick_index)
+        feature = self.rc_config.get_stick_feature(stick_index)
+        v = stick_value
+
+        if feature['average']:
+            v = self.average_stick_value(op_index, v)
+        
+        if feature['conversion'] is not None:
+            v = self.cubic_stick_value(v, feature['conversion']['paramA'], feature['conversion']['paramB'], feature['conversion']['paramC'])
+
+        if feature['valueInverse']:
+            v = -v
+
+        if feature['discretize'] is not None:
+            v = self.discretized_stick_value(v, feature['discretize'])
+
+        return v
+    
+    def switch_event(self, switch_index: int, down: bool) -> bool:
+        """
+        Handle switch events.
+        
+        Returns True if the event is triggered. 
+        For 'push': Returns True on down -> up transition.
+        For 'toggle': Toggles the state on each down event.
+        """
+        feature = self.rc_config.get_switch_feature(switch_index)
+
+        # Determine the event_on state based on the feature's 'on' condition
+        if feature['on'] == 'down':
+            event_on = down
+        else:
+            event_on = not down
+
+        event_triggered = False
+
+        if feature['type'] == 'push':
+            # Push event is triggered on down -> up transition
+            previous_state = self.switch_states.get(switch_index, False)
+            if previous_state is True and event_on is False:
+                event_triggered = True
+            self.switch_states[switch_index] = event_on
+        
+        elif feature['type'] == 'toggle':
+            # Toggle event toggles the state on each down event
+            if event_on and not self.switch_states.get(switch_index, False):
+                self.switch_states[switch_index] = True
+            elif event_on and self.switch_states[switch_index]:
+                self.switch_states[switch_index] = False
+            event_triggered = self.switch_states[switch_index]
+        else:
+            # Non-toggle, non-push events just return the current state
+            event_triggered = event_on
+
+        return event_triggered
+
+
+    def average_stick_value(self, op_index, new_value: float):
+        """
+        履歴を使用してスティックの平均値を計算する
+        """
+        history = self.stick_history[op_index]
+        history.append(new_value)
+        if len(history) > self.history_len:
+            history.pop(0)
+        return sum(history) / len(history)
+    
+    def cubic_stick_value(self, x: float, a_value: float, b_value: float, c_value: float = 0.0, d_value: float = 0.0) -> float:
+        """
+        ドローンのスティック操作を3次関数で計算し、正規化する関数。
+        """
+        # 3次関数の計算
+        y = a_value * x**3 + b_value * x**2 + c_value * x + d_value
+
+        # 出力を -1 から 1 の範囲に制限（クリッピング）
+        y_clipped = max(min(y, 1.0), -1.0)
+
+        return y_clipped
+
+
+    def discretized_stick_value(self, value, split_value: float):
+        return round(value / split_value) * split_value
