a12948b0640026bde4c262f680a291455fa80ecd
kenjihiranabe <kenji.hiranabe@change-vision.com>
2024-10-18 18:18:32 +0900

    test visualization

--- drone_physics/plot.py
+++ drone_physics/plot.py
@@ -8,18 +8,20 @@ omega_times_series = pd.read_csv('omega_times_series.csv')
 fig, ax1 = plt.subplots()
 
 # Create a line plot for ACC
-color = 'tab:blue'
 ax1.set_xlabel('TIME')
-ax1.set_ylabel('ACC', color=color)
-ax1.plot(omega_times_series['TIME'], omega_times_series['ACC'], color=color)
-ax1.tick_params(axis='y', labelcolor=color)
+ax1.set_ylabel('ACC', color='tab:blue')
+ax1.plot(omega_times_series['TIME'], omega_times_series['ACC(a)'], color='tab:blue', label='acc(a)')
+ax1.plot(omega_times_series['TIME'], omega_times_series['ACC(b)'], color='tab:green', label='acc(b)')
+ax1.legend()
+ax1.tick_params(axis='y', labelcolor='tab:blue')
 
 # Create a second y-axis for OMEGA
 ax2 = ax1.twinx()
-color = 'tab:red'
-ax2.set_ylabel('OMEGA', color=color)
-ax2.plot(omega_times_series['TIME'], omega_times_series['OMEGA'], color=color)
-ax2.tick_params(axis='y', labelcolor=color)
+ax2.set_ylabel('OMEGA', color='tab:red')
+ax2.plot(omega_times_series['TIME'], omega_times_series['OMEGA(a)'], color='tab:red', label='omega(a)')
+ax2.plot(omega_times_series['TIME'], omega_times_series['OMEGA(b)'], color='tab:pink', label='omega(b)')
+ax2.legend()
+ax2.tick_params(axis='y', labelcolor='tab:red')
 
 fig.tight_layout()
 plt.show()
