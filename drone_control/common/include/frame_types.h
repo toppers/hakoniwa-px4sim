edd5e83696342b6b6c0be38af0701fa5dd354fed
Takashi Mori <kanetugu2015@gmail.com>
2024-12-17 08:07:12 +0900

    fixed compilation error for windows

--- drone_control/common/include/frame_types.h
+++ drone_control/common/include/frame_types.h
@@ -1,6 +1,7 @@
 #ifndef _FRAME_TYPES_HPP_
 #define _FRAME_TYPES_HPP_
 
+#include "drone_physics.hpp"
 #define DEGREE2RADIAN(v)    ( (v) * M_PI / (180.0) )
 #define RADIAN2DEGREE(v)    ( (180.0 * (v)) / M_PI )
 #define RPM2EULER_RATE(v)   ( ((v) * 2 * M_PI) / 60.0 )
