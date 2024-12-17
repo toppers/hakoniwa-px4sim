7b4f6a8a39eb150ba1cd201801c5f1c8fe77ebfc
Takashi Mori <kanetugu2015@gmail.com>
2024-12-17 09:03:11 +0900

    fixed compilation error for windows

--- drone_control/common/include/frame_types.h
+++ drone_control/common/include/frame_types.h
@@ -1,11 +1,14 @@
 #ifndef _FRAME_TYPES_HPP_
 #define _FRAME_TYPES_HPP_
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
+#ifdef WIN32
+#define _USE_MATH_DEFINES
 #endif
+#include <cmath>
+//#ifndef M_PI
+//#define M_PI 3.14159265358979323846
+//#endif
 
-// M_PI を使えるようになります
 #define DEGREE2RADIAN(v)    ( (v) * M_PI / (180.0) )
 #define RADIAN2DEGREE(v)    ( (180.0 * (v)) / M_PI )
 #define RPM2EULER_RATE(v)   ( ((v) * 2 * M_PI) / 60.0 )
