e7b198b93fe2b425309ce505307771e817825092
Takashi Mori <kanetugu2015@gmail.com>
2024-12-20 08:45:37 +0900

    warn fixed

--- refactor/cmake-options/win-cmake-options.cmake
+++ refactor/cmake-options/win-cmake-options.cmake
@@ -1,18 +1,21 @@
-# Define NOUSE_HAKO_MASTER macro
-add_definitions(-DNOUSE_HAKO_MASTER)
+# Define build options
+add_definitions(-DNOUSE_HAKO_MSTER)
 
-# Compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wunknown-pragmas -Wmissing-prototypes -Wtrigraphs -Wimplicit-int")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic -fPIC")
+# Compiler settings
+if(MSVC)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W3")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /EHsc")
+else()
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99 -Wall -Wunknown-pragmas -Wmissing-prototypes -Wtrigraphs -Wimplicit-int")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-long-long -pedantic -fPIC")
+endif()
 
-# C++ Standard
+# C++ standard
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 # Build type settings
-set(BUILD_TYPE "release")
-if (debug)
-    set(BUILD_TYPE "debug")
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O0")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0")
 else()
@@ -20,16 +23,9 @@ else()
 endif()
 
 # Platform-specific settings
-if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-    # Windows-specific compiler and linker options
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W3 /DWIN32 /D_WINDOWS")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /DWIN32 /D_WINDOWS")
-    add_compile_definitions(WIN32 TRUE)
-elseif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    # macOS-specific options
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     add_compile_definitions(MACOSX TRUE)
-else()
-    # Linux/Unix-specific options
+elseif(UNIX)
     set(CMAKE_THREAD_LIBS_INIT "-lpthread")
     set(CMAKE_HAVE_THREADS_LIBRARY 1)
     set(CMAKE_USE_WIN32_THREADS_INIT 0)
@@ -37,11 +33,12 @@ else()
     set(THREADS_PREFER_PTHREAD_FLAG ON)
 endif()
 
-# GCOV settings for code coverage
-set(GCOV "disabled")
-if (gcov)
+# Coverage settings
+if(gcov AND NOT MSVC)
     set(GCOV "enabled")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
     set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --coverage")
+else()
+    set(GCOV "disabled")
 endif()
