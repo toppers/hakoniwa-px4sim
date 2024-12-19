13103e5e241731ecc5efcb8154c58611ed5b8840
Takashi Mori <kanetugu2015@gmail.com>
2024-12-20 08:44:12 +0900

    warn fixed

--- refactor/cmake-options/win-cmake-options.cmake
+++ refactor/cmake-options/win-cmake-options.cmake
@@ -1,34 +1,35 @@
-add_definitions(-DNOUSE_HAKO_MSTER)
+# Define NOUSE_HAKO_MASTER macro
+add_definitions(-DNOUSE_HAKO_MASTER)
 
-set(CMAKE_C_FLAGS "-std=gnu99")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wunknown-pragmas")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wmissing-prototypes")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wtrigraphs")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wimplicit-int")
+# Compiler flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wunknown-pragmas -Wmissing-prototypes -Wtrigraphs -Wimplicit-int")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic -fPIC")
 
+# C++ Standard
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-long-long")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
 
+# Build type settings
 set(BUILD_TYPE "release")
 if (debug)
     set(BUILD_TYPE "debug")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O0")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0")
 else()
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2")
 endif()
 
-if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+# Platform-specific settings
+if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    # Windows-specific compiler and linker options
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W3 /DWIN32 /D_WINDOWS")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /DWIN32 /D_WINDOWS")
+    add_compile_definitions(WIN32 TRUE)
+elseif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    # macOS-specific options
     add_compile_definitions(MACOSX TRUE)
 else()
+    # Linux/Unix-specific options
     set(CMAKE_THREAD_LIBS_INIT "-lpthread")
     set(CMAKE_HAVE_THREADS_LIBRARY 1)
     set(CMAKE_USE_WIN32_THREADS_INIT 0)
@@ -36,6 +37,7 @@ else()
     set(THREADS_PREFER_PTHREAD_FLAG ON)
 endif()
 
+# GCOV settings for code coverage
 set(GCOV "disabled")
 if (gcov)
     set(GCOV "enabled")
