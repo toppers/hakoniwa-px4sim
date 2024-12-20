b11b44f649ee445f0b47ed139eb349e2e58f2437
Takashi Mori <kanetugu2015@gmail.com>
2024-12-21 07:39:19 +0900

    moved mavlink codes

--- refactor/mavlink/mavlink_config.hpp
+++ refactor/mavlink/mavlink_config.hpp
@@ -4,13 +4,8 @@
 #define MAVLINK_CONFIG_CHAN_0           0
 #define MAVLINK_CONFIG_CHAN_1           1
 #define MAVLINK_SAVE_PACKET_NUM         100
-#if 0
-#define MAVLINK_CONFIG_SYSTEM_ID        0x0
-#define MAVLINK_CONFIG_COMPONENT_ID     0x0
-#else
 #define MAVLINK_CONFIG_SYSTEM_ID        142
 #define MAVLINK_CONFIG_COMPONENT_ID     42
-#endif
 
 #define MAVLINK_CONFIG_VERSION          3
 
