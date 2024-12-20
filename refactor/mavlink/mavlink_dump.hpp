785b7af037d94495112d41add22edf8148e6b3bc
Takashi Mori <kanetugu2015@gmail.com>
2024-12-21 08:30:33 +0900

    add dump

--- refactor/mavlink/mavlink_dump.hpp
+++ refactor/mavlink/mavlink_dump.hpp
@@ -4,11 +4,13 @@
 #include "mavlink.h"
 #include "mavlink_msg_types.hpp"
 
-extern bool px4_data_hb_received;
-extern bool px4_data_long_received;
 
-extern void mavlink_msg_dump(mavlink_message_t &msg);
-extern void mavlink_message_dump(MavlinkDecodedMessage &message);
-
+namespace hako::mavlink {
+class MavlinkDump {
+public:
+    static void mavlink_header_dump(mavlink_message_t &msg);
+    static void mavlink_message_dump(MavlinkDecodedMessage &message);
+};
+} // namespace hako::mavlink
 
 #endif /* _MAVLINK_DUMP_HPP_ */
--- refactor/mavlink/mavlink_msg_types.hpp
+++ refactor/mavlink/mavlink_msg_types.hpp
@@ -8,7 +8,7 @@ typedef enum {
     MAVLINK_MSG_TYPE_UNKNOWN,
     MAVLINK_MSG_TYPE_HEARTBEAT,
     MAVLINK_MSG_TYPE_LONG,
-    MAVLINK_MSG_TYPE_ACK,
+    MAVLINK_MSG_TYPE_COMMAND_ACK,
     MAVLINK_MSG_TYPE_HIL_SENSOR,
     MAVLINK_MSG_TYPE_HIL_STATE_QUATERNION,
     MAVLINK_MSG_TYPE_SYSTEM_TIME,
@@ -22,7 +22,7 @@ typedef struct {
     union {
         mavlink_heartbeat_t heartbeat;
         mavlink_command_long_t command_long;
-        mavlink_command_ack_t ack;
+        mavlink_command_ack_t command_ack;
         mavlink_hil_sensor_t sensor;
         mavlink_hil_state_quaternion_t hil_state_quaternion;
         mavlink_system_time_t system_time;
