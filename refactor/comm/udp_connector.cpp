db01914d4c152d328a442ebf5bbedc103fa1d0a1
Takashi Mori <kanetugu2015@gmail.com>
2024-12-21 18:49:57 +0900

    bug fixed

--- refactor/comm/udp_connector.cpp
+++ refactor/comm/udp_connector.cpp
@@ -70,6 +70,7 @@ bool UdpCommIO::send(const char* data, int datalen, int* send_datalen) {
 }
 
 bool UdpCommIO::close() {
+    ::shutdown(sockfd, SHUT_RDWR);
     if(sockfd >= 0) {
         close_socket(sockfd); // プラットフォームごとに正しい関数を使用
         sockfd = -1;
