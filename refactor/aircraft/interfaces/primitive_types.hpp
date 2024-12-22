58a3339c583ec729fe065a29fb9aedd46c246023
Takashi Mori <kanetugu2015@gmail.com>
2024-12-22 14:31:13 +0900

    add M_PI

--- refactor/aircraft/interfaces/primitive_types.hpp
+++ refactor/aircraft/interfaces/primitive_types.hpp
@@ -8,6 +8,11 @@
 namespace hako::aircraft {
 
 const double GRAVITY = 9.81;
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 /*
  * Coordinate System: Ground Coordinate System (NED)
