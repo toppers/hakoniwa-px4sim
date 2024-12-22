6b16e30e9bc0f3c61eb864ca2fa10798d484a1fe
Takashi Mori <kanetugu2015@gmail.com>
2024-12-22 09:41:09 +0900

    bug fixed

--- refactor/logger/ilog.hpp
+++ refactor/logger/ilog.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <cstdint>
+#include <variant>
 
 namespace hako::logger {
 
@@ -27,7 +28,6 @@ typedef struct {
     LogType type;
 } LogHeaderType;
 
-#include <variant>
 
 typedef std::variant<bool, int8_t, uint8_t, int16_t, uint16_t, 
                      int32_t, uint32_t, int64_t, uint64_t, 
